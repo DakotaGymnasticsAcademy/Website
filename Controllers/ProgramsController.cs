eb50358794de54c02d5153cec6c463257067d949
Greg Peter-Joyal <gregpj@imaginet.com>
2019-09-26 22:01:43 -0500

    initial checkin

---
+++ Controllers/ProgramsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DakotaGymnasticsAcademy.Controllers
+{
+    public class ProgramsController : Controller
+    {
+        // GET: About
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FieldTrips()
+        {
+            return View();
+        }
+
+        public ActionResult Camps()
+        {
+            return View();
+        }
+
+        public ActionResult Birthdays()
+        {
+            return View();
+        }
+    }
+}
